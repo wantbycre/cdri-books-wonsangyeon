7a8dedf1376704dfbd3242f9761cf10250424851
wantbycre <wantbycre@gmail.com>
2026-01-15 11:40:40 +0900

    fix: query 캐싱 api 추가

--- src/hooks/useBooks.ts
+++ src/hooks/useBooks.ts
@@ -7,5 +7,7 @@ export const useBookSearch = (params: SearchParams) => {
         queryKey: ["books", params],
         queryFn: () => fetchBooks(params),
         enabled: !!params.query,
+        staleTime: 1000 * 60 * 5, // 5분간 데이터 유효
+        gcTime: 1000 * 60 * 10, // 10분간 캐시 유지
     });
 };
